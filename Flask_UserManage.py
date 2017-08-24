10020ff94162193278b0bcf86cecf32a827410a2
smnra <hppall@163.com>
2017-08-24 09:31:31 +0800

    添加留言板

--- Flask_UserManage.py
+++ Flask_UserManage.py
@@ -46,6 +46,20 @@ def entry():
     return  render_template('entry.html',entries = entryList, form = myForm )
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
